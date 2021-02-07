557679b8e09a0497cd91cd562cc6f588a10a185e
Piotr Michna <pm@piotrmichna.pl>
2021-02-07 17:35:07 +0100

    class twitter na potrzeby testow, dodanie .idea/ do .gitignore

---
+++ twitter.py
@@ -0,0 +1,10 @@
+class Twitter(object):
+    version = '1.0'
+
+    def __init__(self):
+        self.tweets = []
+
+    def tweet(self, message):
+        if len(message)>160:
+            raise Exception('Message too long.')
+        self.tweets.append(message)
